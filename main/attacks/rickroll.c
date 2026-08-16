6422995280fdcc42bdb5b1a666622ddbb66a6d76
Hussein <mnasg32@gmail.com>
2026-08-16 19:30:57 +0300

    Update rickroll.c

--- main/attacks/rickroll.c
+++ main/attacks/rickroll.c
@@ -1,13 +1,8 @@
 #include "esp_wifi.h"
-#include "esp_http_server.h"
 #include <string.h>
+#include "esp_log.h"
 
-static esp_err_t rick_handler(httpd_req_t *req) {
-    httpd_resp_set_type(req, "text/html");
-    httpd_resp_send(req, "<html><meta http-equiv='refresh' content='0;url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'></html>", 
-                    HTTPD_RESP_USE_STRLEN);
-    return ESP_OK;
-}
+static const char *TAG = "RICKROLL";
 
 void rickroll_start(void) {
     wifi_config_t ap_config = {0};
@@ -20,15 +15,5 @@ void rickroll_start(void) {
     esp_wifi_set_config(WIFI_IF_AP, &ap_config);
     esp_wifi_start();
     
-    httpd_handle_t server = NULL;
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    httpd_start(&server, &config);
-    
-    httpd_uri_t uri = {
-        .uri = "/",
-        .method = HTTP_GET,
-        .handler = rick_handler,
-        .user_ctx = NULL
-    };
-    httpd_register_uri_handler(server, &uri);
+    ESP_LOGI(TAG, "Rickroll AP started!");
 }
