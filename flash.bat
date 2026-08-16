b191091ee7e28eb70a04d469916848c6f99f1304
Hussein <mnasg32@gmail.com>
2026-08-16 19:23:00 +0300

    Update flash.bat

--- flash.bat
+++ flash.bat
@@ -1,1 +1,5 @@
-
+batch
+@echo off
+esptool.py --chip esp32 --port COM3 erase_flash
+esptool.py --chip esp32 --port COM3 write_flash -z 0x1000 build\bootloader\bootloader.bin 0x8000 build\partition_table\partition-table.bin 0x10000 build\PredatoryESP32.bin
+pause
