36a580c62e70351e4f0daf82e5b87a73c75eb484
Hussein <mnasg32@gmail.com>
2026-08-16 18:07:42 +0300

    Create probe_sniff.c

---
+++ attacks/probe_sniff.c
@@ -0,0 +1,18 @@
+#include "esp_wifi.h"
+#include "esp_log.h"
+
+static const char *TAG = "SNIFF";
+
+void probe_callback(void *buf, wifi_promiscuous_pkt_type_t type) {
+    wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t *)buf;
+    uint8_t *data = pkt->payload;
+    if (data[0] == 0x40) {
+        ESP_LOGI(TAG, "Probe: %02X:%02X:%02X:%02X:%02X:%02X",
+                 data[10], data[11], data[12], data[13], data[14], data[15]);
+    }
+}
+
+void start_sniffing(void) {
+    esp_wifi_set_promiscuous(true);
+    esp_wifi_set_promiscuous_rx_cb(probe_callback);
+}
